90736bd8414d51a271d9b0c7a02af8202e7324c1
Jeremy White <jcwhite414@gmail.com>
2023-05-30 22:39:16 -0500

    SSR in progress

--- Moxie-Client/pages/skills/index.tsx
+++ Moxie-Client/pages/skills/index.tsx
@@ -1,5 +1,41 @@
-const Skills = () => {
-    return <h1>Skills Page</h1>
+import SkillCard from "@/components/cards/SkillCard"
+import Container from "@/components/layout/Container"
+import { routeConstants } from "@/utils/config";
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import Link from "next/link"
+
+export const getServerSideProps: GetServerSideProps<{
+    skills: Skill[];
+}> = async () => {
+    const res = await fetch(`${routeConstants.apiUrl}/skillTree`);
+    const skills = await res.json();
+    return { props: { skills } };
+};
+
+const Skills = ({ skills }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    return <>
+
+
+        <Link href="/">
+            <Container header="Skills">
+
+                {skills?.map(skill => <SkillCard skill={skill} key={skill.id} />)}
+            </Container>
+
+
+        </Link>
+
+    </>
+}
+
+export type Skill = {
+    id: string,
+    userId: string,
+    proficiencyLevel: string,
+    experiencePoints: number,
+    availableSkillPoints: number,
+    dateCreated: string,
+    dateLastModified: string
 }
 
 export default Skills
