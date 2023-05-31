7a31ee73324e793137435948bf51a5702026ab70
Jeremy White <jcwhite414@gmail.com>
2023-05-30 22:36:31 -0500

    Basic skill card

---
+++ Moxie-Client/components/cards/SkillCard.tsx
@@ -0,0 +1,52 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Button from "../interaction/Button"
+import Link from "next/link"
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import useHoverDelay from "@/utils/hooks/useHoverDelay";
+import dateUtils from "@/utils/dateUtils";
+import { Skill } from "@/pages/skills";
+
+
+const SkillCard = ({ skill }: Props) => {
+    const [hover, setHover] = useHoverDelay()
+
+
+    return (<div className="flex flex-col  border border-gray-200 rounded-lg shadow-md md:flex-row md:w-full dark:border-gray-700 dark:bg-gray-800" onMouseEnter={() => setHover(true)}>
+        <div className="h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg text-8xl flex items-center justify-around" >
+
+            <FontAwesomeIcon icon={faDumbbell} bounce={hover} style={{ color: "#ffffff", }} />
+        </div>
+        <div className="flex flex-col justify-between p-4 leading-normal w-3/4">
+            <div className="flex flex-col md:flex-row items-start justify-between">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Title
+                </h5>
+                <p className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-gray-400">
+                    Last change: {dateUtils.formatDistance(skill.dateLastModified)}
+
+                </p>
+            </div>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Body
+            </p>
+
+
+            <div className="flex gap-6 md:flex-row justify-end">
+
+                <Link href="/this">
+                    <Button type="button">
+                        View more Info
+                    </Button>
+                </Link>
+
+
+            </div>
+        </div>
+    </div>)
+}
+
+type Props = {
+    skill: Skill
+}
+export default SkillCard
